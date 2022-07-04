66feccfe017b561dfa4cd8c84777b780e87eefb3
Adrian DeRose <adrian.derose6@gmail.com>
2022-07-04 17:37:06 -0500

    feat(Home): Featured Properties design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Featured from '../../components/Featured/Featured';
+import FeaturedProperties from '../../components/FeaturedProperties/FeaturedProperties';
 import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar';
 import PropertyList from '../../components/PropertyList/PropertyList';
@@ -13,7 +14,11 @@ const Home = () => {
 				<Featured />
 				<h1 className='homeTitle'>Browse by property type</h1>
 				<PropertyList />
-				<h1 className='homeTitle'>Homes guests love</h1>
+				<h1 className='homeTitle'>
+					More than just hotels... Bookers discover pure comfort with homes,
+					apartments, and more
+				</h1>
+				<FeaturedProperties />
 			</div>
 		</div>
 	);
