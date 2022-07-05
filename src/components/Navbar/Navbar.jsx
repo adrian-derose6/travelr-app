5e533329c533c8f1db69c1d6dbc22b37d72e6d6f
Adrian DeRose <adrian.derose6@gmail.com>
2022-07-05 01:42:52 -0500

    fix: Navbar and Footer styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlane } from '@fortawesome/free-solid-svg-icons';
 import './Navbar.css';
 
 const Navbar = () => {
 	return (
 		<div className='navbar'>
 			<div className='navContainer'>
-				<span className='logo'>Travelr</span>
+				<span className='logo'>
+					<FontAwesomeIcon icon={faPlane} />
+					Travelr.com
+				</span>
 				<div className='navItems'>
 					<button className='navButton'>Register</button>
 					<button className='navButton'>Login</button>
