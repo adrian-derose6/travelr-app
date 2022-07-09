b7d6b608ae9aa3ac99c3e42c48c67badd05e455b
Adrian DeRose <adrian.derose6@gmail.com>
2022-07-09 07:14:45 -0500

    feat: create Login page connected to Auth context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Hotel from './pages/Hotel/Hotel';
 import List from './pages/List/List';
+import Login from './pages/Login/Login';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 				<Route path='/' element={<Home />} />
 				<Route path='/hotels' element={<List />} />
 				<Route path='/hotels/:hotelId' element={<Hotel />} />
+				<Route path='/login' element={<Login />} />
 			</Routes>
 		</BrowserRouter>
 	);
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+import { useAuth } from '../../context/AuthContext';
+import { API_URL } from '../../config/env-vars';
+import './Login.css';
+
+const Login = () => {
+	const [credentials, setCredentials] = useState({
+		username: undefined,
+		password: undefined,
+	});
+	const { loading, error, dispatch } = useAuth();
+
+	const handleChange = (e) => {
+		setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+	};
+
+	const handleClick = async (e) => {
+		e.preventDefault();
+		dispatch({ type: 'LOGIN_START' });
+		try {
+			const res = await axios.post(`${API_URL}/auth/login`, credentials);
+			dispatch({ type: 'LOGIN_SUCCESS', payload: res.data });
+		} catch (err) {
+			dispatch({ type: 'LOGIN_FAILURE', payload: err.response.data });
+		}
+	};
+
+	return (
+		<div className='login'>
+			<div className='lContainer'>
+				<input
+					type='text'
+					placeholder='username'
+					id='username'
+					onChange={handleChange}
+					className='lInput'
+				/>
+				<input
+					type='password'
+					placeholder='password'
+					id='password'
+					onChange={handleChange}
+					className='lInput'
+				/>
+				<button className='lButton'>Login</button>
+				{error && <span>{error.message}</span>}
+			</div>
+		</div>
+	);
+};
+export default Login;
