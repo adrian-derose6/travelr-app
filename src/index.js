1c767067dd4266239b1543f516ea320e51d4e2db
Adrian DeRose <adrian.derose6@gmail.com>
2022-07-09 06:56:25 -0500

    feat(auth): create Auth Context

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { SearchContextProvider } from './context/SearchContext';
+import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<SearchContextProvider>
-			<App />
-		</SearchContextProvider>
+		<AuthProvider>
+			<SearchContextProvider>
+				<App />
+			</SearchContextProvider>
+		</AuthProvider>
 	</React.StrictMode>
 );
